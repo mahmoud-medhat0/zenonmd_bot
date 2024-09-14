517841d05b7aef6fa84a504b4dc74f37eac0a425
mohamedeldony3 <60990149+mohamedeldony3@users.noreply.github.com>
2024-09-14 04:06:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["201061419002", 'Mohamed Elsony', true],["201020306059", 'FLEX', true]], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
+["201061419002", 'Mohamed Elsony', true],["201020306059", 'FLEX', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
 ]
 
 global.mods = []
